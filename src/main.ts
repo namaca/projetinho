e341be3f3b0e6c69b4b2c7b2acddf42b0b7acf9d
unknown <honoratopaula1@gmail.com>
2024-05-03 09:09:23 -0300

    test

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,11 @@ import { Client } from "https://deno.land/x/postgres/mod.ts";
 
 Deno.serve({ port: 3000 }, async (req) => {
     console.log(req.headers.get('host'))
+
+    // Verifique o caminho do URL da solicitação
+    const url = new URL(req.url, `http://${req.headers.get('host')}/`);
+    const path = url.pathname;
+
     const client = new Client({
         user: Deno.env.get("DB_USER"),
         database: Deno.env.get("DB_NAME"),
@@ -12,39 +17,42 @@ Deno.serve({ port: 3000 }, async (req) => {
 
     await client.connect();
     
-    {
+    let html = "";
 
-    const result = await client.queryArray("SELECT * FROM alunos");
-    console.log(result.rows); // [[1, 'Carlos'], [2, 'John'], ...]
+    // Verifique o caminho do URL e responda de acordo
+    if (path === "/alunos") {
+        const result = await client.queryArray("SELECT * FROM alunos");
+        console.log(result.rows); // [[1, 'Carlos'], [2, 'John'], ...]
+
+        html = `
+        <html>
+            <head>
+                <title>Alunos</title>
+            </head>
+            <body>
+                <h1>Alunos</h1>
+                <ul>
+                    ${result.rows.map((row) => `<li>${row[1]}</li>`).join("")}
+                </ul>
+            </body>
+        </html>
+        `;
+    } else {
+        // Se o caminho do URL não corresponder a "/alunos", responda com uma mensagem de erro ou outra coisa
+        html = "<html><head><title>Erro 404</title></head><body><h1>Erro 404 - Página não encontrada</h1></body></html>";
+    }
+
     await client.end();
-   
-    const html = `
-    <html>
-        <head>
-        <title>Alunos</title>
-        </head>
-        <body>
-        <h1>Alunos</h1>
-        <ul>
-            ${result.rows.map((row) => `<li>${row[1]}</li>`).join("")}
-        </ul>
-        </body>
-    </html>
-    `;
-
+    
     const response = new Response(html, {
         status: 200,
         headers: {
           "content-type": "text/html",
         },
-      });
-    
-      console.log(response.status); // 200
-      console.log(response.headers.get("content-type")); // text/html
-    
-      return response;
+    });
 
-    }
-    
-    
+    console.log(response.status); // 200
+    console.log(response.headers.get("content-type")); // text/html
+
+    return response;
 });
