5f3d06e35a91546a408dd6b7e1c67bff621ad835
unknown <honoratopaula1@gmail.com>
2024-05-03 00:03:08 -0300

    testeee

--- src/index.ts
+++ src/index.ts
@@ -3,23 +3,20 @@ import { load } from "https://deno.land/std@0.224.0/dotenv/mod.ts";
 
 const env = await load();
 
-Deno.serve({ port: 3000 }, async () => {
-
+console.log(env["DB_USER"])
     const client = new Client({
-        user: env["DB_USER"],
-        database: env["DB_NAME"],
-        hostname: env["DB_HOST"],
-        port: 5432,
-        password: env["DB_PASSWORD"],
+    user: env["DB_USER"],
+    database: env["DB_NAME"],
+    hostname: env["DB_HOST"],
+    port: 5432,
+    password: env["DB_PASSWORD"],
     });
-    
     await client.connect();
-    
-    {
+  
+  {
     const result = await client.queryArray("SELECT * FROM alunos");
     console.log(result.rows); // [[1, 'Carlos'], [2, 'John'], ...]
-    }
-    
-    
-    await client.end();
-});
+  }
+  
+  
+  await client.end();
