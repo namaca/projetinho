cf2fa82c1374c6abab0a696b3ec62711911157aa
unknown <honoratopaula1@gmail.com>
2024-05-03 07:50:22 -0300

    test

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { Client } from "https://deno.land/x/postgres/mod.ts";
 
 Deno.serve({ port: 3000 }, async (req) => {
-    let result = {};
+    
     const client = new Client({
         user: Deno.env.get("DB_USER"),
         database: Deno.env.get("DB_NAME"),
@@ -11,11 +11,27 @@ Deno.serve({ port: 3000 }, async (req) => {
     });
 
     await client.connect();
+    
+    {
 
-    result = await client.queryArray("SELECT * FROM alunos");
+    const result = await client.queryArray("SELECT * FROM alunos");
     console.log(result.rows); // [[1, 'Carlos'], [2, 'John'], ...]
-
     await client.end();
+   
+    
+    const response = new Response("<html> Hello </html>", {
+        status: 200,
+        headers: {
+          "content-type": "text/html",
+        },
+      });
+    
+      console.log(response.status); // 200
+      console.log(response.headers.get("content-type")); // text/html
+    
+      return response;
 
-    req.respond({ body: JSON.stringify(result.rows) });
+    }
+    
+    
 });
